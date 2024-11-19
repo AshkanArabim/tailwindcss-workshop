337f64b5ef4347c6ca6bd5fca5ff39d9e9ba54ed
AshkanArabim <ashkan.arabim@gmail.com>
2024-11-19 10:23:11 -0700

    remove class names

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-red-600 text-white">
-      <div className="text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4 lg:text-8xl">We are FOSS Club.</h1>
-        <p className="text-lg md:text-xl mb-6">
+    <div>
+      <div>
+        <h1>We are FOSS Club.</h1>
+        <p>
           The most awesome CS organization on campus.
         </p>
         <a
           href="https://github.com/FOSS-club-UTEP/tailwindcss-workshop"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-gray-950 hover:drop-shadow-lg hover:shadow-black hover:bg-gray-600 text-white py-2 px-4 rounded transition duration-200"
         >
           See the code on GitHub
         </a>
